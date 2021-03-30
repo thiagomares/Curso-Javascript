98b412ff4dabf071f4a8553efd1b3251038562d2
thiagomares <39134260+thiagomares@users.noreply.github.com>
2021-03-30 20:28:14 -0300

    For in e For Of

--- Construtor For/For In.js
+++ Construtor For/For In.js
@@ -4,22 +4,21 @@
     Este formato do for é basicamente o mesmo for do python, baseando-se em um array ao invés de todos os sets do laço serem declarados
 */
 
-let array = [1, 2, 3, 4, 5]
-let i
+let array = [1, 2, 3, 4, 5];
+let i;
 
-for (i in array)
-{
-    console.log(array[i])
+for (i in array) {
+  console.log(array[i]);
 }
 
 // Utilizando objetos
 
 let pessoa = {
-    nome: 'Thiago',
-    sobrenome: 'Mares',
-    idade: 25
-}
+  nome: "Thiago",
+  sobrenome: "Mares",
+  idade: 25,
+};
 
 for (let chaves in pessoa) {
-    console.log(chaves, pessoa[chaves])
+  console.log(chaves, pessoa[chaves]);
 }
