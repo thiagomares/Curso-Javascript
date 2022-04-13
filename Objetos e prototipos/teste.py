ac14450a9de1e5e27cc35dad006f6085e40176df
thiagomares <39134260+thiagomares@users.noreply.github.com>
2022-04-13 15:49:36 -0300

    teste em python

---
+++ Objetos e prototipos/teste.py
@@ -0,0 +1,11 @@
+array = [
+    {'Nome': "Thiago", 'Sobrenome' : 'Mares'},
+    {'Nome': "Ana Clara", 'Sobrenome' : 'Mares'}
+]
+nome = "Mara"
+sobrenome = 'Ferreira'
+array.append({"Nome": nome, "Sobrenome": sobrenome})
+print(array)
+
+
+    
